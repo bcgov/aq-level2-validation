78989e4b64714aeed51b5e09a0e001bdb3a02fc1
Haga <Donna.Haga@gov.bc.ca>
2021-03-02 12:40:29 -0700

    Launching point for 2020 validation

--- _02-compileBooks.R
+++ _02-compileBooks.R
@@ -1,18 +1,20 @@
 # compile annual data validation reports for all files in regionalPreppedData:
 
+#THIS ISN'T WORKING, DATA ISN'T CHANGING BETWEEN DIFFERENT REGIONS/FILES.
+
 root<-stringr::str_remove(dir("./regionalPreppedData"),
                           ".rds")
 
-purrr::walk(root[1],
+purrr::walk(root,
             
-            function(root){
+            function(r){
               
               bookdown::render_book("_AnnualDataValidation.Rmd", 
                                     "bookdown::gitbook",
-                                    output_dir = stringr::str_c("_",root,sep = ""),
+                                    output_dir = stringr::str_c("_",r,sep = ""),
                                     params = list(
                                       preppedData = stringr::str_c("./regionalPreppedData/",
-                                                                   root,
+                                                                   r,
                                                                    ".rds", sep = ""),
                                       year = 2019
                                     )
